167efa58c4552194406e146a5e6848f3b7de66f2
nban22 <nban22@clc.fitus.edu.vn>
2024-03-12 15:31:06 +0700

    add lib of venv

--- myvenv/Scripts/activate.bat
+++ myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Nguye\Desktop\tmp\fsm\-\File-system-management\myvenv
+set VIRTUAL_ENV=C:\Users\Nguye\Desktop\tmp\fsm\File-system-management\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
